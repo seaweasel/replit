ac338cb1270a7e366189f602f3a9c39e15e57fc9
seaweasel <jrodperalta@gmail.com>
2022-04-27 00:51:50 +0000

    Converted complexity table for different data structures into object/hash

--- first.js
+++ first.js
@@ -1,1 +1,64 @@
-// hi
+//hi mom
+
+const lines = [
+  "Array	Θ(1)	Θ(n)	Θ(n)	Θ(n)	O(1)	O(n)	O(n)	O(n)	O(n)",
+  "Stack	Θ(n)	Θ(n)	Θ(1)	Θ(1)	O(n)	O(n)	O(1)	O(1)	O(n)",
+  "Queue	Θ(n)	Θ(n)	Θ(1)	Θ(1)	O(n)	O(n)	O(1)	O(1)	O(n)",
+  "Singly-Linked List	Θ(n)	Θ(n)	Θ(1)	Θ(1)	O(n)	O(n)	O(1)	O(1)	O(n)",
+  "Doubly-Linked List	Θ(n)	Θ(n)	Θ(1)	Θ(1)	O(n)	O(n)	O(1)	O(1)	O(n)",
+  "Skip List	Θ(log(n))	Θ(log(n))	Θ(log(n))	Θ(log(n))	O(n)	O(n)	O(n)	O(n)	O(n log(n))",
+  "Hash Table	N/A	Θ(1)	Θ(1)	Θ(1)	N/A	O(n)	O(n)	O(n)	O(n)",
+  "Binary Search Tree	Θ(log(n))	Θ(log(n))	Θ(log(n))	Θ(log(n))	O(n)	O(n)	O(n)	O(n)	O(n)",
+  "Cartesian Tree	N/A	Θ(log(n))	Θ(log(n))	Θ(log(n))	N/A	O(n)	O(n)	O(n)	O(n)",
+  "B-Tree	Θ(log(n))	Θ(log(n))	Θ(log(n))	Θ(log(n))	O(log(n))	O(log(n))	O(log(n))	O(log(n))	O(n)",
+  "Red-Black Tree	Θ(log(n))	Θ(log(n))	Θ(log(n))	Θ(log(n))	O(log(n))	O(log(n))	O(log(n))	O(log(n))	O(n)",
+  "Splay Tree	N/A	Θ(log(n))	Θ(log(n))	Θ(log(n))	N/A	O(log(n))	O(log(n))	O(log(n))	O(n)",
+  "AVL Tree	Θ(log(n))	Θ(log(n))	Θ(log(n))	Θ(log(n))	O(log(n))	O(log(n))	O(log(n))	O(log(n))	O(n)",
+  "KD Tree	Θ(log(n))	Θ(log(n))	Θ(log(n))	Θ(log(n))	O(n)	O(n)	O(n)	O(n)	O(n)"
+];
+
+allComplexities = {};
+const operations = ['Access', 'Search', 'Insertion', 'Deletion'];
+const cases = ['Average', 'Worst'];
+
+for(let line of lines){
+  line = line.split('\t');
+  let i = 0;
+  let title = line[0];
+
+  let complexities = {};
+  
+  for(const timeCase of cases){
+    for(const operation of operations){
+      i += 1;
+      
+      if(!(operation in complexities)){
+        complexities[operation] = {};
+      }
+      complexities[operation][timeCase] = line[i];
+    }
+
+  }
+  // add space complexity
+  i += 1;
+  complexities['Space (worst)'] = line[i];
+  
+  allComplexities[title] = complexities;
+}
+
+console.log(allComplexities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
